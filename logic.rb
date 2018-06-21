ae0571008e1a86b11d309bf640781bd61025ea51
Isabella Duran <2020ikd7@gmail.com>
2018-06-21 17:12:07 +0000

    automatically backed up by learn

--- logic.rb
+++ logic.rb
@@ -1,9 +1,9 @@
-def game(laugh)
- if laugh == "yes"
-   puts "you're out!"
- end 
- if laugh == "no"
-   puts "you're in!"
- end
-end
-game("no")
+# def game(laugh)
+# if laugh == "yes"
+#   puts "you're out!"
+# end 
+# if laugh == "no"
+#   puts "you're in!"
+# end
+# end
+# game("no")
