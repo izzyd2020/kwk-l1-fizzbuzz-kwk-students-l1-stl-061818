12a901442fe4c5f7eb0f4a9a1bcb43851c8d4364
Isabella Duran <2020ikd7@gmail.com>
2018-06-21 17:16:12 +0000

    automatically backed up by learn

--- logic.rb
+++ logic.rb
@@ -1,21 +1,13 @@
-# def game(laugh)
-# if laugh == "yes"
-#   puts "you're out!"
-# end 
-# if laugh == "no"
-#   puts "you're in!"
-# end
-# end
-# game("no")
-def animal(month)
- if month == "May"
-   puts "you're a Fox!"
+ def game(laugh)
+ if laugh == "yes"
+   puts "you're out!"
  end 
- if month == "june"
-   puts "you're a Blob fish!"
+ if laugh == "no"
+   puts "you're in!"
  end
-end
-animal("may")
+ end
+game("no")
+
 
 
 
